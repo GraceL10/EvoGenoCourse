fd37cc78fcd3bdbfa9339513fb5a28b81911bed8
Grace Lawson <glawson@umass.edu>
2020-08-31 11:34:04 -0400

    Lab 1 files

---
+++ Lawson_Lab1.R
@@ -0,0 +1,36 @@
+### Exercise 1
+x <- 3
+y <- 15
+
+# sum x + y
+x + y
+
+# difference of x and y
+x - y
+
+# product of x and y
+x * y 
+
+# quotient of x and y
+x / y
+
+
+### Exercise 2
+x <- 2
+y <- 3
+x^5 > y^4
+
+### Exercise 3
+x <- c(22,62,148,43,129)
+
+# sum of vector
+sum (x)
+
+# divide vector by sum
+x / sum(x)
+
+### Exercise 4
+DNA <- c("A","T","C","G")
+
+# sort DNA vector
+sort(DNA)
